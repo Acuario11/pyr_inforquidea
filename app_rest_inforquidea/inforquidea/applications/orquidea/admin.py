a952d5629e0e7b1dcf8d5816b264211a2ad87ce8
Acuario11 <madaichuco@upeu.edu.pe>
2021-10-14 13:21:53 -0500

    ver t completa

--- app_rest_inforquidea/inforquidea/applications/orquidea/admin.py
+++ app_rest_inforquidea/inforquidea/applications/orquidea/admin.py
@@ -2,4 +2,15 @@ from django.contrib import admin
 
 from . import models
 # Register your models here.
-admin.site.register(models.Orquidea)
+#admin.site.register(models.Orquidea)
+@admin.register(models.Orquidea)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = [
+        'id',
+        'tipo_crecimiento',
+        'nombre_comun',
+        'floracion',
+        'duracion_flor',
+        'tamaño_flor',
+        'ubicacion',
+    ]
--- app_rest_inforquidea/inforquidea/applications/orquidea/models.py
+++ app_rest_inforquidea/inforquidea/applications/orquidea/models.py
@@ -2,13 +2,13 @@ from django.db import models
 
 # Create your models here.
 class Orquidea(models.Model):
-    tipo_crecimiento= models.CharField(max_length=30, unique=True, null=True)
-    nombre_comun=models.CharField(max_length=30, unique=True, null=True)
-    floracion=models.CharField(max_length=30, unique=True, null=True)
-    duracion_flor=models.CharField(max_length=30, unique=True, null=True)
-    tamaño_flor=models.CharField(max_length=30, unique=True, null=True)
-    ubicacion=models.CharField(max_length=60, unique=True, null=True)
+    tipo_crecimiento= models.CharField(max_length=30)
+    nombre_comun=models.CharField(max_length=30, unique=True)
+    floracion=models.CharField(max_length=30)
+    duracion_flor=models.CharField(max_length=30)
+    tamaño_flor=models.CharField(max_length=30)
+    ubicacion=models.CharField(max_length=60)
 
     def __str__(self):
-        return {self.nombre_comun} 
+        return f'{self.nombre_comun}' 
         
