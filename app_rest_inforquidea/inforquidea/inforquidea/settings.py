b37bb624ad5f683024c9e825ffe992d44748b0f4
wagner <wagner.vargas@upeu.edu.pe>
2021-10-19 21:05:23 -0500

    v1

--- app_rest_inforquidea/inforquidea/inforquidea/settings.py
+++ app_rest_inforquidea/inforquidea/inforquidea/settings.py
@@ -40,7 +40,8 @@ INSTALLED_APPS = [
     'drf_yasg',
     #
     'applications.user',
-    'applications.orquidea'
+    'applications.orquidea',
+    'applications.vivero',
 ]
 
 MIDDLEWARE = [
--- app_rest_inforquidea/inforquidea/inforquidea/urls.py
+++ app_rest_inforquidea/inforquidea/inforquidea/urls.py
@@ -44,6 +44,7 @@ schema_view = get_schema_view(
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('applications.orquidea.urls')),
+    path('', include('applications.vivero.urls')),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('docs/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
